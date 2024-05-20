da690319acc35e2160142e7ec7baf0fdabc454d9
mobassherkhandakar <mobassherkhandakar@gmail.com>
2024-05-20 12:02:26 +0600

    this project is started now

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,28 +1,59 @@
-import winston from 'winston';
-import 'winston-mongodb';
-import config from '../config';
+/* eslint-disable no-undef */
+import path from 'path';
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+const { combine, timestamp, label, printf } = format;
 
-const MongoTransport = new winston.transports.MongoDB({
-  db: config.database_url as string,
-  collection: 'logs',
-  options: { useUnifiedTopology: true },
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
+//Customm Log Format
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp);
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} } [${label}] ${level}: ${message}`;
 });
 
-const ConsoleTransport = new winston.transports.Console({
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
-  ),
+const logger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
 });
 
-export const logger = winston.createLogger({
-  transports: [ConsoleTransport, MongoTransport],
+const errorlogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'phu-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
 });
 
-// if (config.node_env !== 'production') {
-//   logger.remove(MongoTransport);
-// }
+export { errorlogger, logger };
