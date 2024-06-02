fff1eff5208346a7a82446c941a9bf888829fb53
mobassherkhandakar <mobassherkhandakar@gmail.com>
2024-06-02 21:26:02 +0600

    academic department route modifi

--- src/app/modules/academicDepartment/academicDepartment.routes.ts
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { AcademicDepartmentController } from './academicDepartment.controller';
 const router = express.Router();
 // router.get('/', AcademicDepartmentController.getAllFromDB);
-// router.get('/:id', AcademicDepartmentController.getDataById);
+// router.get('/:id', AcademicDepartmentController);
 router.post('/', AcademicDepartmentController.InsertIntoDB);
 
 export const AcademicDepartmentRouter = router;
