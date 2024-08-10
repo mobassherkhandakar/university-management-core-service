4847ca8de9f40ce8794339618627efb4bd0562ac
mobassherkhandakar <mobassherkhandakar@gmail.com>
2024-08-10 17:26:03 +0600

    building added and room create

--- src/app/modules/students/student.route.ts
+++ src/app/modules/students/student.route.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { StudentController } from './student.controller';
 const router = express.Router();
 
-router.post('/', StudentController.insertIntoDB);
+router.post('/', auth(ENUM_USER_ROLE.ADMIN), StudentController.insertIntoDB);
 
 export const StudentRouter = router;
